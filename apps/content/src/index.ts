90f418ee8b3d205e9b6610a9bc4345e3bb694bff
Antony Monge Lopez <antony.mongelopez@ucr.ac.cr>
2025-11-10 20:33:11 -0600

    feat: add single-spa integration for content application

---
+++ apps/content/src/index.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOMClient from 'react-dom/client';
+import singleSpaReact from 'single-spa-react';
+import App from './App';
+
+export const { bootstrap, mount, unmount } = singleSpaReact({
+  React,
+  ReactDOMClient,
+  rootComponent: App,
+  errorBoundary() {
+    return <div>Error loading content application</div>;
+  },
+});
